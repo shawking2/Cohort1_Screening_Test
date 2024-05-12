0ac87a9e15887912b2eba864105add12d34a9576
shawking2 <shawking@shawkings-MacBook-Air.local>
2024-05-12 13:29:39 +0700

    first

---
+++ exercise1.py
@@ -0,0 +1,54 @@
+import random
+import bisect
+
+def generate_random_list(n: int):
+	#generate list of 2n unique positive intergers
+	list_n = []
+	while len(list_n) < 2*n:	#2n element
+		random_number = random.randint(1, 2**n -1)	#maximum n bits
+	#generate random number k
+		if random_number not in list_n:
+			bisect.insort(list_n, random_number)
+	k = random.randint(1, 2**n -1)
+	return k, list_n
+
+def binary_search(k: int, n: int, list_n: list[int]):
+	low = 0
+	high = 2*n -1
+	while low <= high:
+		mid = (low+high) // 2
+		# if the middle element is x,return its position. 
+		if list_n[mid] == k:
+			return mid
+		elif list_n[mid] < k:
+			low = mid + 1
+		else:
+			high = mid - 1
+	return -1
+def search_k(k: int, n: int, list_n: list[int]):
+	result = binary_search(k, n, list_n)
+	if result != -1:
+		print("true " + str(result + 1))
+	else:
+		print("false")
+	return result
+def less_than_k(k: int, position: int, list_n: list[int]):
+	list_less_than = []
+	position -= 1
+	if position >= 0:
+		while(position >= 0):
+			list_less_than.append(list_n[position])
+			position -= 1
+		print(list_less_than, k)
+	else:
+		print("nothing")
+
+def main():
+	n = abs(int(input("Enter the number of bits (n must be a positive value): ")))
+	k, list_n = generate_random_list(n)
+	print(str(k)+",", list_n)
+	position = search_k(k, n, list_n)
+	less_than_k(k, position, list_n)
+
+if __name__ == "__main__":
+	main()
