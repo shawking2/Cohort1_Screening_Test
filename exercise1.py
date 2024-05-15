4f9b1510cd8f1846e5bdc4edeadf8ef5f48a00c6
shawking2 <61406160+shawking2@users.noreply.github.com>
2024-05-15 15:25:30 +0700

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -4,20 +4,22 @@ import bisect
 def generate_random_list(n: int):
 	#generate list of 2n unique positive intergers
 	list_n = []
+	list_orginal = []
 	while len(list_n) < 2*n:	#2n element
 		random_number = random.randint(1, 2**n -1)	#maximum n bits
 	#generate random number k
 		if random_number not in list_n:
+			list_orginal.append(random_number)
 			bisect.insort(list_n, random_number)
 	k = random.randint(1, 2**n -1)
-	return k, list_n
+	return k, list_n, list_orginal
 
 def binary_search(k: int, n: int, list_n: list[int]):
 	low = 0
-	high = 2*n -1
+	high = n -1
 	while low <= high:
 		mid = (low+high) // 2
-		# if the middle element is x,return its position. 
+		# if the middle element is k,return its position. 
 		if list_n[mid] == k:
 			return mid
 		elif list_n[mid] < k:
@@ -45,9 +47,9 @@ def less_than_k(k: int, position: int, list_n: list[int]):
 
 def main():
 	n = abs(int(input("Enter the number of bits (n must be a positive value): ")))
-	k, list_n = generate_random_list(n)
+	k, list_n, list_orginal = generate_random_list(n)
 	print(str(k)+",", list_n)
-	position = search_k(k, n, list_n)
+	position = search_k(k, 2*n, list_n)
 	less_than_k(k, position, list_n)
 
 if __name__ == "__main__":
